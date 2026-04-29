87c39a49af7e399ccb884e7e7c754dc2cc52da62
ravari-a <128918114+ravari-a@users.noreply.github.com>
2026-04-29 03:48:06 +0000

    Deploying to gh-pages from @ ravari-a/ravari-a.github.io@889c948f0c6ff77e6427ce052c51cb4a40e81806 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,8 +17,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "cv",
+          description: "A summary of my education, research, and experience. Click the icon at the top right for a PDF version.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
